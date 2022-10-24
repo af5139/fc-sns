03dd9b3825ea9cf895adefa82722d45f42666c5c
woong <gi5060@naver.com>
2022-10-24 18:15:39 +0900

    post save

--- src/main/java/com/fastcampus/sns/service/UserService.java
+++ src/main/java/com/fastcampus/sns/service/UserService.java
@@ -5,7 +5,9 @@ import com.fastcampus.sns.exception.SnsApplicationException;
 import com.fastcampus.sns.model.User;
 import com.fastcampus.sns.model.entity.UserEntity;
 import com.fastcampus.sns.repository.UserEntityRepository;
+import com.fastcampus.sns.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +20,16 @@ public class UserService {
 
     private final UserEntityRepository userEntityRepository;
     private final BCryptPasswordEncoder encoder;
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+    @Value("${jwt.token.expired-time-ms}")
+    private Long expiredTimeMs;
+
+    public User loadUserByUserName(String userName){
+        return userEntityRepository.findByUserName(userName).map(User::fromEntity).orElseThrow(()->
+                new SnsApplicationException(ErrorCode.USER_NOT_FOUND,String.format("%s not founded",userName)));
+    }
     @Transactional
     public User join(String userName,String password){
         //회원가입하려는 userName으로 회원가입된 user가 있는지 있으면 exception 발생
@@ -36,11 +48,13 @@ public class UserService {
         // 회원가입 여부 체크
         UserEntity userEntity = userEntityRepository.findByUserName(userName).orElseThrow(()->new SnsApplicationException(ErrorCode.USER_NOT_FOUND,String.format("%s not founded", userName)));
         // 비밀번호 체크
-        if(!userEntity.getPassword().equals(password)){
-            throw new SnsApplicationException(ErrorCode.DUPLICATED_USER_NAME,"");
+        if(!encoder.matches(password, userEntity.getPassword())){
+        //if(!userEntity.getPassword().equals(password)){
+            throw new SnsApplicationException(ErrorCode.INVALID_PASSWORD);
         }
 
         // 토큰 생성
-        return "";
+        String token = JwtTokenUtils.doGenerateToken(userName,expiredTimeMs,secretKey);
+        return token;
     }
 }
--- src/test/java/com/fastcampus/sns/service/UserServiceTest.java
+++ src/test/java/com/fastcampus/sns/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.sns.service;
 
+import com.fastcampus.sns.exception.ErrorCode;
 import com.fastcampus.sns.exception.SnsApplicationException;
 import com.fastcampus.sns.fixture.UserEntityFixture;
 import com.fastcampus.sns.model.entity.UserEntity;
@@ -37,7 +38,7 @@ public class UserServiceTest {
         //mocking
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
         when(encoder.encode(password)).thenReturn("encrypt password");
-        when(userEntityRepository.save(any())).thenReturn(Optional.of(UserEntityFixture.get(userName,password)));
+        when(userEntityRepository.save(any())).thenReturn(UserEntityFixture.get(userName,password));
 
         Assertions.assertDoesNotThrow(()->userService.join(userName,password));
     }
@@ -54,7 +55,8 @@ public class UserServiceTest {
         when(encoder.encode(password)).thenReturn("encrypt password");
         when(userEntityRepository.save(any())).thenReturn(Optional.of(fixture));
 
-        Assertions.assertThrows(SnsApplicationException.class,()->userService.join(userName,password));
+        SnsApplicationException e = Assertions.assertThrows(SnsApplicationException.class,()->userService.join(userName,password));
+        Assertions.assertEquals(ErrorCode.DUPLICATED_USER_NAME,e.getErrorCode());
     }
 
     @Test
@@ -66,6 +68,7 @@ public class UserServiceTest {
 
         //mocking
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
+        when(encoder.matches(password,fixture.getPassword())).thenReturn(true);
 
         Assertions.assertDoesNotThrow(()->userService.login(userName,password));
     }
@@ -78,7 +81,8 @@ public class UserServiceTest {
         //mocking
         when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
 
-        Assertions.assertThrows(SnsApplicationException.class,()->userService.join(userName,password));
+        SnsApplicationException e = Assertions.assertThrows(SnsApplicationException.class,()->userService.login(userName,password));
+        Assertions.assertEquals(ErrorCode.USER_NOT_FOUND,e.getErrorCode());
     }
 
     @Test
@@ -90,8 +94,9 @@ public class UserServiceTest {
         UserEntity fixture = UserEntityFixture.get(userName,password);
 
         //mocking
-        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.empty());
+        when(userEntityRepository.findByUserName(userName)).thenReturn(Optional.of(fixture));
 
-        Assertions.assertThrows(SnsApplicationException.class,()->userService.join(userName,wrongPassword));
+        SnsApplicationException e = Assertions.assertThrows(SnsApplicationException.class,()->userService.login(userName,wrongPassword));
+        Assertions.assertEquals(ErrorCode.INVALID_PASSWORD,e.getErrorCode());
     }
 }
